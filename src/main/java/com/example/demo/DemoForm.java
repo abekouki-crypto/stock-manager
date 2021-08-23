830f8c4056f90137a9977115f60b51aae82586e7
hasegawa1222 <hasegawa@kaigofukutarou.com>
2021-08-23 09:44:42 +0900

    テスト

---
+++ src/main/java/com/example/demo/DemoForm.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class DemoForm {
+
+    private String name;
+
+    private String tel;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
